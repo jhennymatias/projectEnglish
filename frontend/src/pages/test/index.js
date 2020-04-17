cd8d5e871fc6c75bd34bfd8eebf64c6539837485
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-17 19:03:50 -0300

    alterna as palavras do quiz

--- frontend/src/pages/test/index.js
+++ frontend/src/pages/test/index.js
@@ -1,32 +1,41 @@
 import React, {Component} from 'react';
 import api from '../../services/api';
 import './style.css';
+import {Link} from 'react-router-dom';
 
 
 export default class Test extends Component{
     state ={
         pergunta:[],
         respostas: [],
+        cont:(''),
     };
     componentDidMount(){
-        this.loadWords();
+        this.loadWords(0);
     }
 
     Verifica = async(id) =>{
         if(id === this.state.pergunta._id){
             alert("Certa Resposta!");
+            var aux = Math.floor(Math.random() * 10);
+            while (aux >= (this.state.cont)){
+                aux = Math.floor(Math.random() * 10);
+            }
+            console.log(aux)
+            this.loadWords(aux)
+
         }else{
             alert("Errado!")
         }
     }
 
-    loadWords = async()=>{
+    loadWords = async(id_inicial)=>{
         const response = await api.get("/test");
-        const response2 = await api.get("/wordss");
+        const response2 = await api.get(`/words/${response.data[id_inicial]._id}`);
         this.setState({pergunta:response2.data});
         this.setState({respostas:response.data});
-        
-    };
+        this.setState({cont: response.data.length})
+     };
 
     
     render(){
@@ -46,6 +55,10 @@ export default class Test extends Component{
                     </div>
                     ))}
                 </div>
+                <div className="voltar">
+                    <Link className="back-link" to={'/lista'}>Voltar</Link>
+                </div>
+                
             </div>
         )
     }
