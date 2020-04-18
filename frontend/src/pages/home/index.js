dcf2e169e14afdad2cf863982f60d27f9da5cf1d
Jhennifer Matias <jhennifercristinematias@gmail.com>
2020-04-18 13:20:47 -0300

    home

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -7,15 +7,27 @@ import {Link} from 'react-router-dom';
 export default class Home extends Component{
     state ={
         words: [],
+        cont:(''),
     };
     componentDidMount(){
         this.OneWord();
     }
+    geraNumero(){
+        var aux = Math.floor(Math.random() * 10);
+        while (aux >= (this.state.cont)){
+            aux = Math.floor(Math.random() * 10);
+            console.log(aux)
+        }
+        return 3;
+    }
 
     OneWord = async()=>{
-        const response = await api.get("/wordss");
+        const id = await api.get("/test");
+        this.setState({cont: id.data.length})
+        const aux = this.geraNumero()
+        console.log(aux);
+        const response = await api.get(`/words/${id.data[aux]._id}`);
         this.setState({words:response.data})
-
     };
 
     
